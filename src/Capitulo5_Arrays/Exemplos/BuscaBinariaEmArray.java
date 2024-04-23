2297d78c110db14eb72371618242571f35f0c386
Luiz <luizfelipedn57@gmail.com>
2024-04-23 17:47:53 -0300

    Inserindo busca binária em arrays

---
+++ src/Capitulo5_Arrays/Exemplos/BuscaBinariaEmArray.java
@@ -0,0 +1,31 @@
+package Capitulo5_Arrays.Exemplos;
+
+import javax.swing.*;
+import java.util.Arrays;
+//import java.util.Random;
+
+public class BuscaBinariaEmArray {
+    public static void main (String[] args) {
+        int[] numeros = new int[10000];
+        String mensagem = "Numero não encontrado!";
+        boolean flag = false;
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = (int) (Math.random() * 1000);
+            /*Random random = new Random();
+            numeros[i] = random.nextInt();*/
+        }
+        int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Insira um número inteiro menor que 1000: "));
+        for (int i = 0; i < numeros.length && !flag; i++) {
+            if (numeros[i] == numero) {
+                mensagem = "Numero encontrado na posição: " + i;
+                flag = true;
+            }
+        }
+
+        System.out.println(mensagem);
+
+        Arrays.sort(numeros);
+        mensagem = "Numero encontrado na nova posição: " + Arrays.binarySearch(numeros, numero);
+        System.out.println(mensagem);
+    }
+}
