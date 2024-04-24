f24e4eb87538668968077f6cd0fd825905d98d2f
Luiz <luizfelipedn57@gmail.com>
2024-04-24 18:45:48 -0300

    Inserindo metodo recursivo

---
+++ src/Capitulo6_Metodos/Exemplos/MetodoREcursivo.java
@@ -0,0 +1,17 @@
+package Capitulo6_Metodos.Exemplos;
+
+public class MetodoREcursivo {
+    public static void main (String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i + "! = " + calcularFatorial(i));
+        }
+    }
+
+    public static long calcularFatorial(long num) {
+        if (num <=1) {
+            return (1);
+        } else {
+            return (num * calcularFatorial(num - 1));
+        }
+    }
+}
