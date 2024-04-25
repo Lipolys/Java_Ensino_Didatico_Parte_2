f7fc3dfe50b583b0420c43213b9a2653829c61f9
Luiz <luizfelipedn57@gmail.com>
2024-04-25 16:01:51 -0300

    Inserindo classe que executa métodos de outras classes

---
+++ src/Capitulo6_Metodos/Exemplos/MetodosExternos.java
@@ -0,0 +1,11 @@
+package Capitulo6_Metodos.Exemplos;
+
+public class MetodosExternos {
+    public static void main (String[] args) {
+        MetodoSemRetorno.imprimirTexto("Imprimindo texto usando um método de outra classe");
+        long soma = Math.round(MetodosComRetorno.somar(10, 20));
+        System.out.println(MetodosComRetorno.mostrarDiaPorExtenso(5));
+        System.out.println(MetodosComRetorno.contarLetrasA("Mancada"));
+        System.out.println(soma);
+    }
+}
