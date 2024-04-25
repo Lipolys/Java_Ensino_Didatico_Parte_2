7336e1b917cd3a0220201414b7e36292c9e5d930
Luiz <luizfelipedn57@gmail.com>
2024-04-25 17:10:40 -0300

    Inserindo classe que possui métodos de manipulação de arrays

---
+++ src/Capitulo6_Metodos/Exemplos/MetodosComArray.java
@@ -0,0 +1,32 @@
+package Capitulo6_Metodos.Exemplos;
+
+import java.util.Arrays;
+
+public class MetodosComArray {
+
+    /**
+     * Recebe um array de numeros reais e retorno o maior deles.
+     * Deve ter pelo menos um valor no array, caso contrário gerará um erro
+     *
+     * @param numeros = os numeros a serem comparados
+     * @return = o maior número
+     */
+    public static double buscarMaior (double[] numeros) {
+        double maior = numeros[0];
+        for (double numero : numeros) {
+            maior = Math.max(maior, numero);
+        }
+        return maior;
+    }
+
+    /**
+     * Recebe um array de palavras e retorna ordenados em ordem alfabética
+     *
+     * @param palavras = as palavras a serem comparadas
+     * @return = o array ordenado
+     */
+    public static String[] ordenarPalavras (String[] palavras) {
+        Arrays.sort(palavras);
+        return palavras;
+    }
+}
