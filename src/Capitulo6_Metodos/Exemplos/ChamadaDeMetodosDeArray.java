e9ffc7b697224bb740167395cfe071a2463a7891
Luiz <luizfelipedn57@gmail.com>
2024-04-25 17:16:39 -0300

    Inserindo classe que manipula os métodos de array

---
+++ src/Capitulo6_Metodos/Exemplos/ChamadaDeMetodosDeArray.java
@@ -0,0 +1,14 @@
+package Capitulo6_Metodos.Exemplos;
+
+public class ChamadaDeMetodosDeArray {
+    public static void main (String[] args) {
+        double[] numeros = {12.4, 3.4, 9, 7.3, 6.6};
+        double maior = MetodosComArray.buscarMaior(numeros);
+        System.out.println("O maior valor é: " + maior);
+
+        String[] nomes = {"Ivone", "Carlos", "Lila", "Alzira", "Bruno", "Flavio", "Rita"};
+        for (String nome:MetodosComArray.ordenarPalavras(nomes)) {
+            System.out.print(nome + ", ");
+        }
+    }
+}
