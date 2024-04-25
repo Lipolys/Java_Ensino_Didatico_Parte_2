22ed7b24688acd771e149bcfed5d17c830b521f7
Luiz <luizfelipedn57@gmail.com>
2024-04-25 17:02:11 -0300

    Inserindo classe de chamada de alguns métodos

---
+++ src/Capitulo6_Metodos/Exemplos/ChamadaDeMetodos.java
@@ -0,0 +1,18 @@
+package Capitulo6_Metodos.Exemplos;
+
+import javax.swing.*;
+
+public class ChamadaDeMetodos {
+    public static void main (String[] args) {
+        String nome = EntradaDeDados.lerString("Insira seu nome: ");
+        int idade = EntradaDeDados.lerInteiro(0, 150, "Insira sua idade: ");
+        float altura = EntradaDeDados.letFloat(0, 2.5f, "Insira sua altura: ");
+        double peso = EntradaDeDados.lerDouble(0, 300, "Insira seu peso: ");
+        String opcao = (EntradaDeDados.SelecionarOpcaoBinaria("Você gosta de Java?")) ? "Eu também gosto de Java!" : "Sinto muito... Já experimentou C#?";
+        JOptionPane.showMessageDialog(null, "Nome: " + nome +
+                "\nIdade: " + idade +
+                "\nAltura: " + altura +
+                "\nPeso: " + peso +
+                "\n" + opcao);
+    }
+}
