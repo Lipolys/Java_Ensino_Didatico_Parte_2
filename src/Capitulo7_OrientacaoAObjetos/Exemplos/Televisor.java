2261adefca0986bcbde02f8cb83ed015190eb385
Luiz <luizfelipedn57@gmail.com>
2024-04-26 17:52:08 -0300

    Inserindo novo pacote do capítulo 7 e alguns exemplos

---
+++ src/Capitulo7_OrientacaoAObjetos/Exemplos/Televisor.java
@@ -0,0 +1,22 @@
+package Capitulo7_OrientacaoAObjetos.Exemplos;
+
+public class Televisor {
+    int volume;
+    int canal;
+
+    public void aumentarVolume(){
+        volume++;
+    }
+
+    public void diminuirVolume() {
+        volume--;
+    }
+
+    public void trocarCanal(int c) {
+        canal = c;
+    }
+
+    public void mostrar () {
+        System.out.println("Volue: " + volume + "\nCanal: " + canal);
+    }
+}
---
+++ src/Capitulo7_OrientacaoAObjetos/Exemplos/UsaTelevisor.java
@@ -0,0 +1,13 @@
+package Capitulo7_OrientacaoAObjetos.Exemplos;
+
+public class UsaTelevisor {
+    public static void main(String[] args) {
+        Televisor tv = new Televisor();
+        tv.canal = 150;
+        tv.volume = 3;
+        tv.aumentarVolume();
+        tv.aumentarVolume();
+        tv.trocarCanal(10);
+        tv.mostrar();
+    }
+}
