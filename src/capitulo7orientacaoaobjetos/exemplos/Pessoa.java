27efa16c71a88452949d9d2b794d1a9daa4b7381
Luiz <luizfelipedn57@gmail.com>
2024-04-29 18:27:29 -0300

    Inserindo uso de herança e especialização

---
+++ src/capitulo7orientacaoaobjetos/exemplos/Pessoa.java
@@ -0,0 +1,13 @@
+package capitulo7orientacaoaobjetos.exemplos;
+
+public class Pessoa {
+    private String nome;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ src/capitulo7orientacaoaobjetos/exemplos/PessoaFisica.java
@@ -0,0 +1,13 @@
+package capitulo7orientacaoaobjetos.exemplos;
+
+public class PessoaFisica extends Pessoa {
+    private String cpf;
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
