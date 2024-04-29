1b307da072c274afc4543bb0593d9837a2ca7305
Luiz <luizfelipedn57@gmail.com>
2024-04-29 17:12:06 -0300

    Adicionando exemplos de polimorfismo com construtores

---
+++ src/Capitulo7_OrientacaoAObjetos/Exemplos/UsaTelevisorConstrutor.java
@@ -0,0 +1,14 @@
+package Capitulo7_OrientacaoAObjetos.Exemplos;
+
+public class UsaTelevisorConstrutor {
+    public static void main (String[] args) {
+        TelevisorConstrutor tv1 = new TelevisorConstrutor();
+        TelevisorConstrutor tv2 = new TelevisorConstrutor(3);
+        TelevisorConstrutor tv3 = new TelevisorConstrutor(3, 150);
+
+
+        tv1.mostrar();
+        tv2.mostrar();
+        tv3.mostrar();
+    }
+}
